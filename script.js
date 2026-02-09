e0acb0f05872eeb5e842905fb7a9b21ddea1e24c
Cristian-Ospina17 <cristianospina882@gmail.com>
2026-02-09 07:10:51 -0500

    Primer commit del proyecto calculadora

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function calcularTotal(cuenta, porcentajePropina) {
+    let propina = cuenta * (porcentajePropina / 100);
+    let total = cuenta + propina;
+    return total;
+}
+
+function mostrarTotal() {
+    let cuenta = Number(document.getElementById("cuenta").value);
+    let porcentaje = Number(document.getElementById("propina").value);
+
+    let total = calcularTotal(cuenta, porcentaje);
+
+    document.getElementById("resultado").textContent =
+        "Total a pagar: $" + total.toFixed(2);
+}
