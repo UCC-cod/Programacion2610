a4e75e70a291410e2635a060c45ff840286d28cf
LonelyDeceive <jmario.carvajalde@gmail.com>
2026-02-16 08:57:05 -0500

    Forms

--- Clase3/app.js
+++ Clase3/app.js
@@ -26,3 +26,70 @@ const last = ul.lastElementChild;
 last.remove();
 console.log(introSelection());
 
+(function events() {
+    const btnAlert = document.getElementById("btn-alert");
+    btnAlert.addEventListener("click",()=>{
+        console.log("Hice click");
+    })
+
+    const box = document.getElementById("hover-box");
+    box.addEventListener("mouseover", ()=>{
+        box.style.background = "yellow";
+        box.textContent = "Mouseover"
+        box.style.color = "black";
+    })
+        box.addEventListener("mouseout", ()=>{
+        box.style.background = "transparent";
+        box.textContent = "Hover aqui";
+        box.style.color = "white";
+    })
+
+    const keyInput = document.getElementById("key-input");
+    const keyOut = document.getElementById("key-output");
+
+    keyInput.addEventListener("keydown", (e)=>{
+        keyOut.textContent = `Tecla ${e.key};`
+    })
+
+    keyInput.addEventListener("keyup", (e)=>{
+        console.log(e);
+    }) 
+})();
+
+(function forms() {
+    const form = document.getElementById("login-form");
+    const errmsg = document.getElementById("form-output")
+
+    function validar({user, clave, rol}) {
+        const errores = [];
+        if (!user || user.trim().length < 3) {
+            errores.push("El usuario debe tener mas de 3 caracteres");
+        }
+        if (!clave || clave.length < 5) {
+            errores.push("La clave debe contener mas de 5 caracteres");
+        }
+        if (!rol) {
+            errores.push("Debe seleccionar un rol")
+        }
+        return errores;
+    }
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const user = document.getElementById("usuario").value;
+        const clave = document.getElementById("clave").value;
+        const rol = document.getElementById("rol").value;
+        const errores = validar({user, clave, rol});
+
+    if (errores.length>0) {
+        errmsg.innerHTML = `<span>Errores: ${errores.join("<br>")}</span>`;
+        errmsg.style.color = "var(--danger)";
+        return;
+    }
+    errmsg.textContent = `Bienvenido: ${rol} ${user}`;
+    errmsg.style.color = "var(--ok)";
+    })
+})();
+
+
+
