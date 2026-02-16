93db0543abd15a6117ec7fb8fb0152ed407dba90
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-02-16 06:53:25 -0500

    DOM - Selección

---
+++ Clase3/app.js
@@ -0,0 +1,17 @@
+//console.log("Hola");
+
+//Selección de elementos
+/*
+function introSelection(){
+    const h3Titulo = document.getElementById("titulo-demo");
+    const selector = document.querySelector("p");
+    const allSelector = document.querySelectorAll("p");
+    const claseSelector = document.getElementsByClassName("tag");
+    const nameSelector = document.getElementsByName("usuario");
+    h3Titulo.textContent = "Este es un texto nuevo"
+    h3Titulo.innerHTML = "Este es un texto nuevo con <strong>HTML</strong>"
+    return h3Titulo;  
+}
+
+console.log(introSelection());
+*/
