437fb5370750dd6423755cc6b18dcaa65b0de050
Cristian-Ospina17 <cristianospina882@GMAIL.COM>
2026-02-21 10:22:58 -0500

    Funcionalidad hecho y eliminar agregada

---
+++ app.js
@@ -0,0 +1,51 @@
+(function toDoApp(){
+    const form = document.getElementById("todo-form");
+    const input = document.getElementById("todo-input");
+    const list = document.getElementById("todo-list");
+
+    function crearItemToDo(texto){
+        const container = document.createElement("div");
+        container.className = "todo-item";
+
+        const label = document.createElement("span");
+        label.textContent = texto;
+
+        const containerAcciones = document.createElement("div");
+        containerAcciones.className = "row";
+
+        const btnDone = document.createElement("button");
+        btnDone.type = "button";
+        btnDone.className = "btn";
+        btnDone.textContent = "Hecho";
+
+        const btnDelete = document.createElement("button");
+        btnDelete.type = "button";
+        btnDelete.className = "btn";
+        btnDelete.textContent = "Eliminar";
+
+        btnDone.addEventListener("click", () => {
+            label.style.textDecoration = "line-through";
+        });
+
+        btnDelete.addEventListener("click", () => {
+            container.remove();
+        });
+
+        containerAcciones.append(btnDone, btnDelete);
+        container.append(label, containerAcciones);
+        return container;
+    }
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const texto = input.value.trim();
+
+        if(texto === "") return; 
+
+        const item = crearItemToDo(texto);
+        list.prepend(item);
+
+        input.value = ""; 
+    });
+
+})();
