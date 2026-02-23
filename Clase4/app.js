0f9b02513306d85cf8e71aafea5df14385d8a9b8
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-02-23 07:54:54 -0500

    Peticiones con async await

---
+++ Clase4/app.js
@@ -0,0 +1,29 @@
+//Peticiones
+
+async function obtenerPokemon(nombre) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${nombre}`
+
+    try {
+
+        //Petición GET con Fetch
+        const respuesta = await fetch(url);
+        //console.log("Respuesta", respuesta);
+
+        //Manejando el error
+        if(!respuesta.ok) throw new Error(`Error: ${respuesta.status}`)
+        
+        //Manejo los datos de la respuesta
+        const data = await respuesta.json();
+
+        //Probar los datos
+        console.log("Respuesta", data);
+        console.log("Nombre del pokemon: ", data.name);
+        console.log("URL imagen: ", data.sprites.front_default);        
+
+    } catch (error) {
+        
+    }
+    
+}
+
+obtenerPokemon("pikachu");
